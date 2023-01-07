a549690ff6cd38fe9bed22f10b61200d90ec5f74
daniel-j <daniel.j.hede@gmail.com>
2023-01-07 19:07:27 +0100

    improve enum test case
    add anonymous enum

--- tests/tenumconst.nim
+++ tests/tenumconst.nim
@@ -4,11 +4,17 @@ importc:
   path "."
   "tenumconst.h"
 
-doAssert MY_VAR_A.ord == 0
-doAssert MY_VAR_B.ord == 1
-doAssert MY_VAR_C.ord == 2
+
+doAssert my_enum.MY_VAR_A.ord == 0
+doAssert my_enum.MY_VAR_B.ord == 1
+doAssert my_enum.MY_VAR_C.ord == 2
 doAssert MY_VAR_D.ord == 1
 doAssert MY_VAR_E.ord == 0
 
 doAssert MY_VAR_D == MY_VAR_B
 doAssert MY_VAR_E == MY_VAR_A
+
+
+doAssert ANON_A == 0
+doAssert ANON_B == 1
+doAssert ANON_C == -1
