de5b4595aada6d26dd65c47cb3f12ce031ad715a
daniel-j <daniel.j.hede@gmail.com>
2023-01-11 16:25:30 +0100

    update enumconst test

--- tests/tenumconst.h
+++ tests/tenumconst.h
@@ -11,3 +11,25 @@ enum {
     ANON_B,
     ANON_C = -1
 };
+
+enum {
+    ANON_D = 0,
+
+    // this is ignored now
+    ANON_E = 'E'
+};
+
+#include <limits.h>
+
+typedef enum {
+    SHORT = SHRT_MAX
+} enum_short;
+
+typedef enum {
+    WORD = INT_MAX
+} enum_word;
+
+// does not not work
+// typedef enum {
+//     LONG = LONG_MAX
+// } enum_long;
--- tests/tenumconst.nim
+++ tests/tenumconst.nim
@@ -2,6 +2,7 @@ import "../src/futhark"
 
 importc:
   path "."
+  compilerArg "-fshort-enums"
   "tenumconst.h"
 
 
@@ -18,3 +19,11 @@ doAssert MY_VAR_E == MY_VAR_A
 doAssert ANON_A == 0
 doAssert ANON_B == 1
 doAssert ANON_C == -1
+
+doAssert sizeof(ANON_D) == 1
+
+doAssert sizeof(SHORT) == 2
+doAssert SHORT.ord == 0x7fff
+
+doAssert sizeof(WORD) == 4
+doAssert WORD.ord == 0x7fffffff
