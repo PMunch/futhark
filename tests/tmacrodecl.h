5a9eb595cc62c9d0530fae802ae0f5dac2606c9e
djazz <daniel.j.hede@gmail.com>
2023-06-29 08:13:45 +0200

    Float parsing in macros (#75)
    * Float parsing in macros, relies on nim's parseFloat
    
    * use in instead of endsWith
    
    elegantbeef's suggestion
    
    * add zero float to test

---
+++ tests/tmacrodecl.h
@@ -0,0 +1,15 @@
+#define TEST_PLAIN_INT                     (123)
+#define TEST_PLAIN_LONG                    (-2147483648L)
+#define TEST_PLAIN_LONGLONG                (-9223372036854775808LL)
+
+#define TEST_PLAIN_FLOAT_1                 (123.123f)
+#define TEST_PLAIN_FLOAT_2                 (.123f)
+#define TEST_PLAIN_FLOAT_3                 (123.f)
+#define TEST_PLAIN_FLOAT_4                 (-.0f)
+#define TEST_PLAIN_FLOAT_5                 (-0.f)
+#define TEST_PLAIN_FLOAT_6                 (0.0f)
+
+#define TEST_PLAIN_DOUBLE_1                (123.0)
+
+#define TEST_PLAIN_LONG_DOUBLE_1           (100.0L)
+
